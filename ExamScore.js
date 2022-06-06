c34c1c03b3317ad4d1c9ab77041c309dfdc1cf15
Tatysx33 <106815305+Tatysx33@users.noreply.github.com>
2022-06-06 08:43:54 -0600

    Add files via upload
    Javascript code/ html

---
+++ ExamScore.js
@@ -0,0 +1,20 @@
+
+
+// How to know if I pass the test
+
+var examGrade;
+
+examGrade= 70;
+//printf
+
+if (examGrade >= 70 ) {
+console.log( " Congratulations! you have passed");
+}
+
+  else if ( examGrade >= 60 && examGrade <70 ) {
+    console.log( " you need to do sufcient test" );
+    }
+
+  else  {
+    console.log(" Sorry but you have lost the subject" );
+  }
